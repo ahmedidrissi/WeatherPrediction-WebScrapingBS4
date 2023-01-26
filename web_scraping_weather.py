bd4f568c5c8fecd9f52338653f216e3879f00dbb
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2023-01-26 20:33:51 +0100

    update

--- web_scraping_weather.py
+++ web_scraping_weather.py
@@ -1,15 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
-country = "Morocco"
-city = "Casablanca"
+print("---------------------------------------")
+country = input("Country : ")
+city = input("City : ")
 url = f"https://www.google.com/search?q=weather-{city}-{country}"
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
-bloc = doc.find_all(class_="kCrYT")
-print("\n----------------------------------------\n")
-print(bloc[0].text)
-print("\n----------------------------------------\n")
-print(bloc[1].text)
-print("\n----------------------------------------\n")
+try:
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+    blocs = doc.find_all(class_="kCrYT")
+
+    bloc_1 = blocs[0]
+    city = bloc_1.text.split(" / ")[0]
+
+    bloc_2 = blocs[1]
+    text = bloc_2.text.split("\n")
+    t = text[0].split("\xa0")
+    date = t[1][2:]
+    temp = t[0] + t[1][0:2]
+    weather = text[1].replace("weather.com", "")
+
+    print("---------------------------------------------------------------")
+    print(city, "|", date, "|", temp, "|", weather)
+    print("---------------------------------------------------------------")
+
+except Exception as e:
+    print("Error")
