5e2857c5a15d1f13e199e4a837675b9105c79376
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2023-01-26 18:01:24 +0100

    first test

--- web_scraping_weather.py
+++ web_scraping_weather.py
@@ -1,6 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-country = "morocco"
-city = "casablanca"
+country = "Morocco"
+city = "Casablanca"
 url = f"https://www.google.com/search?q=weather-{city}-{country}"
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+bloc = doc.find_all(class_="kCrYT")
+print("\n----------------------------------------\n")
+print(bloc[0].text)
+print("\n----------------------------------------\n")
+print(bloc[1].text)
+print("\n----------------------------------------\n")
