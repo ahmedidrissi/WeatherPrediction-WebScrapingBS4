98ff463f6a5fb0b2b845374c78c27432e3174ed5
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2023-01-27 17:13:35 +0100

    update

--- scraping_weather_from_google.py
+++ scraping_weather_from_google.py
@@ -4,9 +4,39 @@ import requests
 print("---------------------------------------")
 country = input("Country : ")
 city = input("City : ")
-url = f"https://www.google.com/search?q=weather-{city}-{country}"
+print("---------------------------------------")
 
 try:
+    url = f"https://www.timeanddate.com/weather/{country}/{city}"
+
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+    bloc = doc.find(class_="bk-focus")
+
+    bloc_qlook = bloc.find(class_="bk-focus__qlook")
+    current_data = []
+    current_data.append("Temperature: " + bloc_qlook.find(class_="h2").text.replace("\xa0", ""))
+    current_data.append("Weather: " + bloc_qlook.find("p").text)
+    tags = bloc_qlook.find("p").next_sibling.next_sibling
+    for tag in tags:
+        text = tag.text.replace("\xa0", "")
+        if text != "": current_data.append(text)
+    
+    current_data = current_data[:4] + [current_data[4] + current_data[5] + current_data[6]] + current_data[7:]
+
+    bloc_info = bloc.find(class_="bk-focus__info")
+    table = bloc_info.find("tbody")
+    for tag in table:
+        current_data.append(tag.text.replace("\xa0", ""))
+
+    for info in current_data:
+        print(info)
+    
+    print("---------------------------------------")
+    
+except Exception as e:
+    url = f"https://www.google.com/search?q=weather-{city}-{country}"
+    
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
     blocs = doc.find_all(class_="kCrYT")
@@ -25,5 +55,5 @@ try:
     print(city, "|", date, "|", temp, "|", weather)
     print("---------------------------------------------------------------")
 
-except Exception as e:
+except :
     print("Error")
--- scraping_weather_from_timeanddate.py
+++ scraping_weather_from_timeanddate.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 print("---------------------------------------")
-country = "morocco" #input("Country : ")
-city = "rabat" #input("City : ")
+country = input("Country : ")
+city = input("City : ")
 url = f"https://www.timeanddate.com/weather/{country}/{city}"
+print("---------------------------------------")
 
 try:
     result = requests.get(url)
@@ -13,19 +14,24 @@ try:
 
     bloc_qlook = bloc.find(class_="bk-focus__qlook")
     current_data = []
-    current_data.append(bloc_qlook.find(class_="h2").text.replace("\xa0", ""))
-    current_data.append(bloc_qlook.find("p").text)
+    current_data.append("Temperature: " + bloc_qlook.find(class_="h2").text.replace("\xa0", ""))
+    current_data.append("Weather: " + bloc_qlook.find("p").text)
     tags = bloc_qlook.find("p").next_sibling.next_sibling
     for tag in tags:
         text = tag.text.replace("\xa0", "")
         if text != "": current_data.append(text)
+    
+    current_data = current_data[:4] + [current_data[4] + current_data[5] + current_data[6]] + current_data[7:]
 
     bloc_info = bloc.find(class_="bk-focus__info")
     table = bloc_info.find("tbody")
     for tag in table:
         current_data.append(tag.text.replace("\xa0", ""))
+
+    for info in current_data:
+        print(info)
     
-    print(current_data)
+    print("---------------------------------------")
     
 except Exception as e:
     print("Error")
